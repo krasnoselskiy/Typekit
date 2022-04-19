db7aa1055b1534257b085dbcb321ef6ed8af4404
Boris Krasnoselskiy <boris.krasnoselskiy@digitalsuits.co>
2022-04-19 11:03:40 +0300

    Begin tagList

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import TagList from '../tagList';
 
 function App() {
   return (
     <div className="App">
-      app
+      <TagList />
     </div>
   );
 }
---
+++ src/components/tagList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IProps {
+  name?: string
+}
+
+export default function TagList({
+  name
+}: IProps) {
+  return (
+    <div>TagList</div>
+  )
+}
